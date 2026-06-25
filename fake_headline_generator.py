219556aa9b907f242c2d8c58ef977c12092b139a
Harshith Gowda AR <harshith2007gowda@gmail.com>
2026-06-25 21:09:18 +0530

    add: Fake Headline Generator

---
+++ fake_headline_generator.py
@@ -0,0 +1,21 @@
+import random
+
+subjects = ["Virat kohli" , "Prime minister narendra modi" , "Rocking star yash" , "Elon musk" , "A bus driver from banglore"]
+actions = ["smashes" , "launches" , "shoots" , "prepares" , "drives" , "declares" , "writes" , "eats"]
+places_or_things = ["a century" , "a missile" , "inside the parliment" , "a rocket" , "trees" , "on the sky" , "in the water" ]
+
+while True:
+    subject = random.choice(subjects)
+    action = random.choice(actions)
+    place_or_thing = random.choice(places_or_things)
+
+    headline = f"BREAKING NEWS: {subject} {action} {place_or_thing}"  
+
+    print("\n" , headline)
+    print()
+    print("-"*20)
+
+    user_input = input("\nDo you want another headline (yes/no): ").strip().lower()
+    if user_input == "no":
+        break
+print("\nThank you for using the Fake Headline Generator. Have a fun day.")
